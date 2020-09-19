78ee26fc411cba9fe916bd0b898d24a2c11b6925
Eshetu1 <eshemilian@yahoo.com>
2020-09-19 01:42:15 -0500

    Eshetucal

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int firstnumber = 0;
+        int secondnumber = 0;
+        float result = 0;
+        string opretortype = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void acceptnumber(object sender, EventArgs e)
+        {
+            txtResult.Text += (sender as Button).Text;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void acceptsecondnumber(object sender, EventArgs e)
+        {
+            opretortype = (sender as Button).Text;
+            if (firstnumber == 0)
+            {
+                firstnumber = int.Parse(txtResult.Text);
+                txtResult.Text = "";
+            }
+        }
+
+        private void getresult(object sender, EventArgs e)
+        {
+            secondnumber= int.Parse(txtResult.Text);
+
+            if (opretortype == "+")
+            {
+                result = firstnumber + secondnumber;
+
+            }
+            else if (opretortype == "x")
+            {
+                result = firstnumber * secondnumber;
+            }
+            else if (opretortype == "/")
+            {
+                result = firstnumber / secondnumber;
+            }
+            else if (opretortype == "-")
+            {
+                result = firstnumber - secondnumber;
+            }
+            txtResult.Text = result.ToString();
+
+        }
+
+        private void clearresult(object sender, EventArgs e)
+        {
+            txtResult.Text = "";
+            firstnumber = 0;
+            secondnumber = 0;
+        }
+    }
+}
